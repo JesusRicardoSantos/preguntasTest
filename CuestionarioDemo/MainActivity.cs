f9ce2f347c45766f95713290698414afd3035c11
jose de jesus ricardo santos <jose.jesus.ricardo.santos@outlook.com>
2018-10-18 13:49:31 -0500

    Listo Muchachos

--- CuestionarioDemo/MainActivity.cs
+++ CuestionarioDemo/MainActivity.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using Android.App;
 using Android.Media;
 using Android.OS;
@@ -7,6 +10,7 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Newtonsoft.Json;
 
 namespace CuestionarioDemo
 {
@@ -24,12 +28,44 @@ namespace CuestionarioDemo
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+            CopyDocuments("baseInterna.sqlite", "LeonaliDB.db");
             txtUsuario = (EditText)FindViewById(Resource.Id.txtUsuario);
             txtPassword = (EditText)FindViewById(Resource.Id.txtContrasena);
             btnLogin = (Button)FindViewById(Resource.Id.btnLogin);
+            var ln = (LinearLayout)FindViewById(Resource.Id.lnPreguntas);
 
             btnLogin.Click += delegate {
-                
+                com.somee.servicioweb1test.Service service = new com.somee.servicioweb1test.Service();
+                var xml = service.Consulta("select * from usuarios where user_name = '" + txtUsuario.Text + "' and user_password = '" + txtPassword.Text + "';");
+
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(ClaseDato));
+                var claseDato = new ClaseDato();
+                try
+                {
+                    var jsonLimpio = "";
+                    var bandera = false;
+
+                    foreach (var item in xml)
+                    {
+                        if (item == '[')
+                            bandera = true;
+                        if (bandera)
+                            jsonLimpio += item;
+                        if (item == ']')
+                            break;
+                    }
+
+                    var results = JsonConvert.DeserializeObject<List<ClaseDato>>(jsonLimpio);
+
+                    new General().GuardarDatosUsuario(results[0].id_user, results[0].user_name, results[0].user_password);
+
+                    StartActivity(typeof(ActivityMenu));
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, "Usuario y/o contraseña son incorrectos", ToastLength.Short).Show();
+                }
+                    
             };
 
             video = (VideoView)FindViewById(Resource.Id.videoPlay);
@@ -48,30 +84,34 @@ namespace CuestionarioDemo
             mp.SetVideoScalingMode(VideoScalingMode.ScaleToFitWithCropping);
         }
 
-        /*
-        public override bool OnCreateOptionsMenu(IMenu menu)
+        public void CopyDocuments(string FileName, string AssetsFileName)
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string dbPath = System.IO.Path.Combine(path, FileName);
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            try
             {
-                return true;
+                if (!File.Exists(dbPath))
+                {
+                    using (var br = new BinaryReader(Application.Context.Assets.Open(AssetsFileName)))
+                    {
+                        using (var bw = new BinaryWriter(new FileStream(dbPath, FileMode.Create)))
+                        {
+                            byte[] buffer = new byte[2048];
+                            int length = 0;
+                            while ((length = br.Read(buffer, 0, buffer.Length)) > 0)
+                            {
+                                bw.Write(buffer, 0, length);
+                            }
+                        }
+                    }
+                }
             }
+            catch
+            {
 
-            return base.OnOptionsItemSelected(item);
+            }
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }*/
     }
 }
 
